f83b9e3986ef990355ae1d11395591e9b301b8c8
Osman Raif Güneş <raif.gunes@robotic.mobi>
2021-01-27 17:05:12 +0300

    Add project files.

---
+++ FaturaLogo/MainForm.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace FaturaLogo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnFaturaSec = new System.Windows.Forms.Button();
+            this.FaturayaLogo = new System.Windows.Forms.WebBrowser();
+            this.openFileDialogFatura = new System.Windows.Forms.OpenFileDialog();
+            this.btnLogoSec = new System.Windows.Forms.Button();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.işlemler = new System.Windows.Forms.GroupBox();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.işlemler.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnFaturaSec
+            // 
+            this.btnFaturaSec.Location = new System.Drawing.Point(6, 19);
+            this.btnFaturaSec.Name = "btnFaturaSec";
+            this.btnFaturaSec.Size = new System.Drawing.Size(158, 23);
+            this.btnFaturaSec.TabIndex = 0;
+            this.btnFaturaSec.Text = "Fatura Seç";
+            this.btnFaturaSec.UseVisualStyleBackColor = true;
+            this.btnFaturaSec.Click += new System.EventHandler(this.btnFaturaSec_Click);
+            // 
+            // FaturayaLogo
+            // 
+            this.FaturayaLogo.Location = new System.Drawing.Point(203, 13);
+            this.FaturayaLogo.MinimumSize = new System.Drawing.Size(20, 20);
+            this.FaturayaLogo.Name = "FaturayaLogo";
+            this.FaturayaLogo.Size = new System.Drawing.Size(1083, 576);
+            this.FaturayaLogo.TabIndex = 1;
+            // 
+            // openFileDialogFatura
+            // 
+            this.openFileDialogFatura.FileName = "openFileDialog1";
+            // 
+            // btnLogoSec
+            // 
+            this.btnLogoSec.Location = new System.Drawing.Point(6, 58);
+            this.btnLogoSec.Name = "btnLogoSec";
+            this.btnLogoSec.Size = new System.Drawing.Size(158, 23);
+            this.btnLogoSec.TabIndex = 2;
+            this.btnLogoSec.Text = "Logo Seç";
+            this.btnLogoSec.UseVisualStyleBackColor = true;
+            this.btnLogoSec.Visible = false;
+            this.btnLogoSec.Click += new System.EventHandler(this.btnLogoSec_Click);
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(64, 132);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth.TabIndex = 3;
+            this.txtWidth.Visible = false;
+            this.txtWidth.TextChanged += new System.EventHandler(this.txtWidth_TextChanged);
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(64, 167);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 3;
+            this.txtHeight.Visible = false;
+            this.txtHeight.TextChanged += new System.EventHandler(this.txtHeight_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Uzunluk";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Yükseklik";
+            this.label2.Visible = false;
+            // 
+            // işlemler
+            // 
+            this.işlemler.Controls.Add(this.btnPrint);
+            this.işlemler.Controls.Add(this.btnFaturaSec);
+            this.işlemler.Controls.Add(this.label2);
+            this.işlemler.Controls.Add(this.btnLogoSec);
+            this.işlemler.Controls.Add(this.label1);
+            this.işlemler.Controls.Add(this.txtWidth);
+            this.işlemler.Controls.Add(this.txtHeight);
+            this.işlemler.Location = new System.Drawing.Point(12, 13);
+            this.işlemler.Name = "işlemler";
+            this.işlemler.Size = new System.Drawing.Size(185, 199);
+            this.işlemler.TabIndex = 6;
+            this.işlemler.TabStop = false;
+            this.işlemler.Text = "groupBox1";
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(6, 97);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(158, 23);
+            this.btnPrint.TabIndex = 6;
+            this.btnPrint.Text = "Yazdır";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Visible = false;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1298, 511);
+            this.Controls.Add(this.işlemler);
+            this.Controls.Add(this.FaturayaLogo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Kafkas oto lastik";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.işlemler.ResumeLayout(false);
+            this.işlemler.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFaturaSec;
+        private System.Windows.Forms.WebBrowser FaturayaLogo;
+        private System.Windows.Forms.OpenFileDialog openFileDialogFatura;
+        private System.Windows.Forms.Button btnLogoSec;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox işlemler;
+        private System.Windows.Forms.Button btnPrint;
+    }
+}
+
---
+++ FaturaLogo/MainForm.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FaturaLogo
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void htmlDocument_Click(object sender, HtmlElementEventArgs e)
+        {
+            HtmlElement element = this.FaturayaLogo.Document.GetElementFromPoint(e.ClientMousePosition);
+            element.Id = "sirketLogoParent";
+            element.Style += "background-color:#efefef;";
+            this.FaturayaLogo.Document.MouseUp -= htmlDocument_Click;
+            btnLogoSec.Visible = true;
+        }
+
+        private void btnFaturaSec_Click(object sender, EventArgs e)
+        {
+
+            openFileDialogFatura.Filter = "fatura dosyası |*.html";
+            if (openFileDialogFatura.ShowDialog() == DialogResult.OK)
+            {
+                if (Path.GetExtension(openFileDialogFatura.FileName) == ".html")
+                {
+                    FaturayaLogo.DocumentText = File.ReadAllText(openFileDialogFatura.FileName);
+                    HtmlDocument document = this.FaturayaLogo.Document;
+                    document.MouseUp += new HtmlElementEventHandler(this.htmlDocument_Click);
+                    MessageBox.Show("Fatura üzerinde logonun eklenecği yere tıklayın. Arka planı gri olacak. Yanlış tıklarsanız tekrar fatura dosyası seçin.");
+                }
+                else
+                {
+                    MessageBox.Show("Sonu html ile biten dosya seçin");
+                }
+            }
+        }
+        public static readonly List<string> ImageExtensions = new List<string> { ".JPG", ".JPE", ".JPEG", ".BMP", ".GIF", ".PNG", ".JFIF" };
+
+        private void btnLogoSec_Click(object sender, EventArgs e)
+        {
+            openFileDialogFatura.Filter = "fatura logo |*.jpg;*.jpeg;*.png;*.gif;*.bmp;*.jfif";
+            if (openFileDialogFatura.ShowDialog() == DialogResult.OK)
+            {
+
+                if (ImageExtensions.Contains(Path.GetExtension(openFileDialogFatura.FileName).ToUpperInvariant()))
+                {
+                    HtmlElement userimage = FaturayaLogo.Document.CreateElement("img");
+                    userimage.SetAttribute("src", openFileDialogFatura.FileName);
+                    userimage.Id = "sirketLogo";
+                    userimage.Style = "width:150px;";
+                    HtmlElement element = FaturayaLogo.Document.GetElementById("sirketLogoParent");
+                    element.AppendChild(userimage);
+                    element.Style = element.Style.Replace("BACKGROUND-COLOR: #efefef", "");
+
+                    label1.Visible = true;
+                    label2.Visible = true;
+                    txtWidth.Visible = true;
+                    txtHeight.Visible = true;
+                    btnPrint.Visible = true;
+                    txtWidth.Text = "150";
+                    txtWidth.Text= "150";
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Resim dosyası seçin");
+                }
+            }
+        }
+
+        private void txtWidth_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(txtWidth.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Please enter only numbers.");
+                txtWidth.Text = txtWidth.Text.Remove(txtWidth.Text.Length - 1);
+            }
+            FaturayaLogo.Document.GetElementById("sirketLogo").Style = "width:" + txtWidth.Text + "px;";
+        }
+
+        private void txtHeight_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(txtHeight.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Please enter only numbers.");
+                txtHeight.Text = txtHeight.Text.Remove(txtHeight.Text.Length - 1);
+            }
+            FaturayaLogo.Document.GetElementById("sirketLogo").Style = "width:" + txtHeight.Text + "px;";
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            FaturayaLogo.Width = Convert.ToInt32(this.Width * 0.80);
+            işlemler.Width = Convert.ToInt32(this.Width * 0.15);
+            FaturayaLogo.Left = işlemler.Right + 10;
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            FaturayaLogo.ShowPrintDialog();
+        }
+    }
+}
+;
